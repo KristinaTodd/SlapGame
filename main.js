69057b68eaa93b7a1245a0de68a661d6f8b1bb0d
Kristina Todd <Kristina.Todd22@gmail.com>
2020-01-30 16:31:15 -0700

    game play updates

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 let shipHealth = 100
-let manPower = 100
+let yourShipHealth = 100
 let damagers = {
-  canon: { damage: 10 },
-  molotov: { damage: 5 },
-  rifle: { damage: 1 }
+  canon: { damage: 7, detriment: 5 },
+  molotov: { damage: 3, detriment: 1 },
+  rifle: { damage: 1, detriment: 0 }
 }
 
 function playGame() {
   document.getElementById("game-area").classList.remove("hidden")
   document.getElementById("play-button").classList.add("hidden")
+
+  setInterval(gameTimer, 900)
 }
+
 
 function damageShip(damager) {
   let damageStrength = damagers[damager].damage
   shipHealth = shipHealth - damageStrength
-  console.log(shipHealth)
-  updateShipHealth()
+
+  updatePirateHealth()
 
   if (shipHealth <= 50) {
     document.getElementById("pirate-ship").classList.add("damaged")
@@ -25,26 +28,83 @@ function damageShip(damager) {
 
 }
 
+function damageYourShip(damager) {
+  let damageStrength = damagers[damager].detriment
+  yourShipHealth = yourShipHealth - damageStrength
+
+  updatePirateHealth()
+
+  if (yourShipHealth <= 50) {
+    document.getElementById("your-ship").classList.add("damaged")
+  }
+
+  loseGame()
+
+}
+
 function winGame() {
-  if (shipHealth == 0) {
+  if (shipHealth <= 0) {
     alert("you win")
     location.reload()
   }
-
 }
 
+function loseGame() {
+  if (yourShipHealth == 0) {
+    alert("you lose")
+    location.reload()
+  }
+}
 
+function gameTimer() {
+  yourShipHealth--
+  updateYourShipHealth()
 
-function updateShipHealth() {
+  if (yourShipHealth <= 50) {
+    document.getElementById("your-ship").classList.add("damaged")
+  }
+
+  loseGame()
+}
+
+function applyBandages() {
+  yourShipHealth = yourShipHealth + 5
+  console.log(yourShipHealth)
+  updateYourShipHealth()
+
+  if (yourShipHealth >= 100) {
+    document.getElementById("bandages").classList.add("invisible")
+
+    setTimeout(bandageTimeOutOver, 4000);
+  }
+}
+
+function updatePirateHealth() {
   document.getElementById("ship-health").innerHTML = `${shipHealth}`
 }
 
-function canonBallTimer() {
-  document.getElementById("time-out").classList.add("invisible")
-
-  setTimeout(timeOutOver, 4000);
+function updateYourShipHealth() {
+  document.getElementById("your-ship-health").innerHTML = `${yourShipHealth}`
 }
 
-function timeOutOver() {
-  document.getElementById("time-out").classList.remove("invisible")
+function canonBallTimer() {
+  document.getElementById("canon-ball").classList.add("invisible")
+  setTimeout(canonTimeOutOver, 4000);
+}
+
+function molotovTimer() {
+  document.getElementById("molotov").classList.add("invisible")
+  setTimeout(molotovTimeOutOver, 2000);
+}
+
+function canonTimeOutOver() {
+  document.getElementById("canon-ball").classList.remove("invisible")
+}
+
+function bandageTimeOutOver() {
+  document.getElementById("bandages").classList.remove("invisible")
+}
+
+function molotovTimeOutOver() {
+  document.getElementById("molotov").classList.remove("invisible")
 }
